3761e814d78d0992a7f3e578db5692bf74516326
YuZhu <yu@keyst.one>
2022-05-06 16:36:11 +0800

    add SolSignRequest type

--- src/main/java/com/sparrowwallet/hummingbird/registry/solana/SolSignRequest.java
+++ src/main/java/com/sparrowwallet/hummingbird/registry/solana/SolSignRequest.java
@@ -17,6 +17,7 @@ public class SolSignRequest extends RegistryItem {
     public static final int DERIVATION_PATH = 3;
     public static final int ADDRESS = 4;
     public static final int ORIGIN = 5;
+    public static final int TYPE = 6;
 
     private final byte[] requestId;
     private final byte[] signData;
@@ -24,30 +25,35 @@ public class SolSignRequest extends RegistryItem {
     private final byte[] address;
     private final String origin;
 
-    public SolSignRequest(byte[] signData, CryptoKeypath derivationPath ) {
+    private final DataType type;
+
+    public SolSignRequest(byte[] signData, CryptoKeypath derivationPath, Integer type) {
         this.requestId = null;
         this.signData = signData;
         this.derivationPath = derivationPath;
         this.address = null;
         this.origin = null;
+        this.type = DataType.fromInteger(type);
     }
 
 
-    public SolSignRequest(byte[] signData, CryptoKeypath derivationPath, byte[] requestId) {
+    public SolSignRequest(byte[] signData, CryptoKeypath derivationPath, byte[] requestId, Integer type) {
         this.requestId = requestId;
         this.signData = signData;
         this.derivationPath = derivationPath;
         this.address = null;
         this.origin = null;
+        this.type = DataType.fromInteger(type);
     }
 
 
-    public SolSignRequest(byte[] signData, CryptoKeypath derivationPath, byte[] requestId, byte[] address, String origin) {
+    public SolSignRequest(byte[] signData, CryptoKeypath derivationPath, byte[] requestId, byte[] address, String origin, Integer type) {
         this.requestId = requestId;
         this.signData = signData;
         this.derivationPath = derivationPath;
         this.address = address;
         this.origin = origin;
+        this.type = DataType.fromInteger(type);
     }
 
     public byte[] getRequestId() {
@@ -70,27 +76,29 @@ public class SolSignRequest extends RegistryItem {
         return address;
     }
 
-    public String getOrigin() {return origin;}
+    public String getOrigin() {
+        return origin;
+    }
 
     @Override
     public RegistryType getRegistryType() {
-        return RegistryType.ETH_SIGN_REQUEST;
+        return RegistryType.SOL_SIGN_REQUEST;
     }
 
     @Override
     public DataItem toCbor() {
         Map map = new Map();
-        if(requestId != null) {
+        if (requestId != null) {
             DataItem uuid = new ByteString(requestId);
             uuid.setTag(37);
             map.put(new UnsignedInteger(REQUEST_ID), uuid);
         }
 
-        if(address != null) {
+        if (address != null) {
             map.put(new UnsignedInteger(ADDRESS), new ByteString(address));
         }
 
-        if(origin != null) {
+        if (origin != null) {
             map.put(new UnsignedInteger(ORIGIN), new UnicodeString(origin));
         }
 
@@ -98,6 +106,7 @@ public class SolSignRequest extends RegistryItem {
         DataItem path = derivationPath.toCbor();
         path.setTag(RegistryType.CRYPTO_KEYPATH.getTag());
         map.put(new UnsignedInteger(DERIVATION_PATH), path);
+        map.put(new UnsignedInteger(TYPE), new UnsignedInteger(type.getTypeIndex()));
         return map;
     }
 
@@ -107,9 +116,10 @@ public class SolSignRequest extends RegistryItem {
         CryptoKeypath derivationPath = null;
         byte[] address = null;
         String origin = null;
+        Integer dataTypeIndex = null;
 
-        Map map = (Map)item;
-        for(DataItem key : map.getKeys()) {
+        Map map = (Map) item;
+        for (DataItem key : map.getKeys()) {
             UnsignedInteger uintKey = (UnsignedInteger) key;
             int intKey = uintKey.getValue().intValue();
             if (intKey == REQUEST_ID) {
@@ -122,21 +132,59 @@ public class SolSignRequest extends RegistryItem {
                 address = ((ByteString) map.get(uintKey)).getBytes();
             } else if (intKey == ORIGIN) {
                 origin = ((UnicodeString) map.get(uintKey)).getString();
+            } else if (intKey == TYPE) {
+                dataTypeIndex = ((UnsignedInteger) map.get(uintKey)).getValue().intValue();
             }
         }
 
-        if(signData == null || derivationPath == null) {
+        if (signData == null || derivationPath == null) {
             throw new IllegalStateException("required data field is missing");
         }
 
-        if(requestId != null && address != null) {
-            return new SolSignRequest(signData, derivationPath, requestId, address, origin);
+        if (requestId != null && address != null) {
+            return new SolSignRequest(signData, derivationPath, requestId, address, origin, dataTypeIndex);
         } else if (requestId != null) {
-            return new SolSignRequest(signData, derivationPath, requestId);
+            return new SolSignRequest(signData, derivationPath, requestId, dataTypeIndex);
         } else if (address != null) {
-            return new SolSignRequest(signData, derivationPath, address);
+            return new SolSignRequest(signData, derivationPath, address, dataTypeIndex);
         } else {
-            return new SolSignRequest(signData, derivationPath);
+            return new SolSignRequest(signData, derivationPath, dataTypeIndex);
+        }
+    }
+
+    public enum DataType {
+        TRANSACTION("sign-type-transaction", 1),
+        MESSAGE("sign-type-message", 2);
+
+        private final String type;
+        private final Integer typeIndex;
+
+        DataType(String type, Integer typeIndex) {
+            this.type = type;
+            this.typeIndex = typeIndex;
+
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public Integer getTypeIndex() {
+            return typeIndex;
+        }
+
+        @Override
+        public String toString() {
+            return type;
+        }
+
+        public static DataType fromInteger(Integer typeIndex) {
+            for (DataType dataType : DataType.values()) {
+                if (dataType.getTypeIndex().equals(typeIndex)) {
+                    return dataType;
+                }
+            }
+            throw new IllegalArgumentException("Unknown sol data type: " + typeIndex);
         }
     }
 }
